627f40f1db4bcc366cc88cdeb7494f7902f2d272
JuniorSelister <junior.selister@gmail.com>
2023-01-24 10:55:12 -0300

    HashTable JDK

---
+++ src/com/selister/sortalgorithms/hashtable/hashtablejdk/HashtableJDKMain.java
@@ -0,0 +1,36 @@
+package com.selister.sortalgorithms.hashtable.hashtablejdk;
+
+import com.selister.sortalgorithms.hashtable.Employee;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class HashtableJDKMain {
+
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+        Map<String, Employee> hashMap = new HashMap<String, Employee>();
+        hashMap.put("Jones", janeJones);
+        hashMap.put("Doe", johnDoe);
+        hashMap.put("Smith", marySmith);
+        Employee employee = hashMap.putIfAbsent("Doe", mikeWilson);
+
+        System.out.println(employee);
+        System.out.println(hashMap.getOrDefault("someone", mikeWilson));
+
+        System.out.println(hashMap.containsKey("Doe"));
+        System.out.println(hashMap.containsValue(janeJones));
+
+//        Iterator<Employee> iterator = hashMap.values().iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+
+        hashMap.forEach((k, v) -> System.out.println("Key = " + k + ", Employee = "+ v));
+     }
+}
