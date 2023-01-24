d51c512a906688bb19e79301f5281c725ff736ac
JuniorSelister <junior.selister@gmail.com>
2023-01-24 10:08:40 -0300

    Chained HashTable

---
+++ src/com/selister/sortalgorithms/hashtable/chaining/ChainedHashtable.java
@@ -0,0 +1,85 @@
+package com.selister.sortalgorithms.hashtable.chaining;
+
+import com.selister.sortalgorithms.hashtable.Employee;
+import com.selister.sortalgorithms.hashtable.StoredEmployee;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class ChainedHashtable {
+
+    private LinkedList<StoredEmployee>[] hashtable;
+
+    public ChainedHashtable() {
+        hashtable = new LinkedList[10];
+
+        for (int i = 0; i < hashtable.length; i++) {
+            hashtable[i] = new LinkedList<StoredEmployee>();
+        }
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        hashtable[hashedKey].add(new StoredEmployee(key, employee));
+    }
+
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+        StoredEmployee employee = null;
+
+        while (iterator.hasNext()) {
+            employee = iterator.next();
+
+            if (employee.key.equals(key)) {
+                return employee.employee;
+            }
+        }
+
+        return null;
+    }
+
+    public Employee remove(String key) {
+        int hashedKey = hashKey(key);
+        int index = -1;
+        ListIterator<StoredEmployee> iterator = hashtable[hashedKey].listIterator();
+        StoredEmployee employee = null;
+
+        while (iterator.hasNext()) {
+            employee = iterator.next();
+            index++;
+
+            if (employee.key.equals(key)) {
+                break;
+            }
+        }
+
+        if (employee == null || !employee.key.equals(key)) {
+            return null;
+        } else {
+            hashtable[hashedKey].remove(index);
+            return employee.employee;
+        }
+    }
+
+    private int hashKey(String key) {
+        return Math.abs(key.hashCode() % hashtable.length);
+    }
+
+    public void printHashtable() {
+        for (int i = 0; i < hashtable.length; i++) {
+            if (hashtable[i] .isEmpty()) {
+                System.out.println("Position " + i + ": empty");
+            } else {
+                System.out.print("Position " + i + ": ");
+                ListIterator<StoredEmployee> iterator = hashtable[i].listIterator();
+
+                while (iterator.hasNext()) {
+                    System.out.print(iterator.next().employee);
+                    System.out.print(" -> ");
+                }
+                System.out.println("null");
+            }
+        }
+    }
+}
