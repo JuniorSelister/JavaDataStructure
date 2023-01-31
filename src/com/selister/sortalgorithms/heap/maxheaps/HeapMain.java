a9fe2bcce3f7bb61aa422cdf9447d4387b123888
JuniorSelister <junior.selister@gmail.com>
2023-01-31 15:56:22 -0300

    Heap - delete

---
+++ src/com/selister/sortalgorithms/heap/maxheaps/HeapMain.java
@@ -0,0 +1,24 @@
+package com.selister.sortalgorithms.heap.maxheaps;
+
+public class HeapMain {
+
+    public static void main(String[] args) {
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+
+        heap.delete(5);
+        System.out.println();
+        heap.printHeap();
+
+    }
+}
